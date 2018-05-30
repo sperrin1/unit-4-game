d540483bd324dd65cdb19002313af5075d43ee33
Reszka <reszka2@gmail.com>
2018-05-30 06:06:00 -0700

    started on javascript, generate target# and put to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,36 @@
-$( document ).ready(function(){
+$(document).ready(function () {
 
-// Setting up random values for each jewel = 1-12
- var crystal1= Math.floor(Math.random()*11+1)
- var crystal2= Math.floor(Math.random()*11+1)
- var crystal3= Math.floor(Math.random()*11+1)
- var crystal4= Math.floor(Math.random()*11+1)
+    // Setting up random values for each jewel = 1-12
+    var crystal1 = Math.floor(Math.random() * 11 + 1)
+    var crystal2 = Math.floor(Math.random() * 11 + 1)
+    var crystal3 = Math.floor(Math.random() * 11 + 1)
+    var crystal4 = Math.floor(Math.random() * 11 + 1)
+    
+    //  generate random number to match and put it in #div3
+    var targetNumber = Math.floor((Math.random() * 101) + 19)
+    $('#targetNumber').text(targetNumber);
+    console.log("target number", targetNumber);
 
-//  generate random number to match and put it in #div6
- math.floor((math.random()*101)+19)
-
-
-
-
-
-
-
-
-
-
-
-
+    //  wins and losses tally keeper for #div4
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    //  Decaring variables for tallies
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+});
