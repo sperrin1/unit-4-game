674ccc82c534dad850826c8da4fa7e8b7a22f112
Reszka <reszka2@gmail.com>
2018-05-30 03:41:14 -0700

    set up buttons in html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$( document ).ready(function(){
+
+// Setting up random values for each jewel = 1-12
+ var crystal1= Math.floor(Math.random()*11+1)
+ var crystal2= Math.floor(Math.random()*11+1)
+ var crystal3= Math.floor(Math.random()*11+1)
+ var crystal4= Math.floor(Math.random()*11+1)
+
+//  generate random number to match and put it in #div6
+ math.floor((math.random()*101)+19)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
