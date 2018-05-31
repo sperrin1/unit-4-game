9febdc9d2b85a3b2071e1f0be2217c582046b34d
Reszka <reszka2@gmail.com>
2018-05-31 14:45:27 -0700

    js mostly there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,82 @@ $(document).ready(function () {
     var playerTotal = 0;
     $('#playerTotal').text(playerTotal);
 
+    //resets the game
+    function reset() {
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        console.log(targetNumber)
+        $('#randomNumber').text(targetNumber);
+        num1 = Math.floor(Math.random() * 11 + 1);
+        num2 = Math.floor(Math.random() * 11 + 1);
+        num3 = Math.floor(Math.random() * 11 + 1);
+        num4 = Math.floor(Math.random() * 11 + 1);
+        playerTotal = 0;
+        $('#finalTotal').text(playerTotal);
+    }
+    //adds the wins to the playerTotal
+    function winna() {
+        alert("You won!");
+        wins++;
+        $('#numberWins').text(wins);
+        reset();
+    }
+    //addes the losses to the playerTotal
+    function loser() {
+        alert("You lose!");
+        losses++;
+        $('#numberLosses').text(losses);
+        reset()
+    }
+
+    //functions for jewel buttons
+    $('#crystal1').on('click', function () {
+        playerTotal = playerTotal + crystal1;
+        console.log("New playerTotal= " + playerTotal);
+        $('#finalTotal').text(playerTotal);
+        //sets win/lose conditions
+        if (playerTotal == targetNumber) {
+            winna();
+        }
+        else if (playerTotal > targetNumber) {
+            loser();
+        }
+    })
+    $('#crystal2').on('click', function () {
+        playerTotal = playerTotal + crystal2;
+        console.log("New playerTotal= " + playerTotal);
+        $('#finalTotal').text(playerTotal);
+        if (playerTotal == targetNumber) {
+            winna();
+        }
+        else if (playerTotal > targetNumber) {
+            loser();
+        }
+    })
+    $('#crystal3').on('click', function () {
+        playerTotal = playerTotal + crystal3;
+        console.log("New playerTotal= " + playerTotal);
+        $('#finalTotal').text(playerTotal);
+        //sets win/lose conditions
+        if (playerTotal == targetNumber) {
+            winna();
+        }
+        else if (playerTotal > targetNumber) {
+            loser();
+        }
+    })
+    $('#crystal4').on('click', function () {
+        playerTotal = playerTotal + crystal4;
+        console.log("New playerTotal= " + playerTotal);
+        $('#finalTotal').text(playerTotal);
+
+        if (playerTotal == targetNumber) {
+            winna();
+        }
+        else if (playerTotal > targetNumber) {
+            loser();
+        }
+    });
+
 
 
 
