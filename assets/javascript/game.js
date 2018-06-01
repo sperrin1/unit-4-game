5a9e1615aaab9f8360576ceb13187e612b3ca083
Reszka <reszka2@gmail.com>
2018-05-31 18:06:00 -0700

    fixed a minor bug with targetNumber

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,10 +25,10 @@ $(document).ready(function () {
         targetNumber = Math.floor(Math.random() * 101 + 19);
         // console.log("new target number", targetNumber);
         $('#targetNumber').text(targetNumber);
-        num1 = Math.floor(Math.random() * 11 + 1);
-        num2 = Math.floor(Math.random() * 11 + 1);
-        num3 = Math.floor(Math.random() * 11 + 1);
-        num4 = Math.floor(Math.random() * 11 + 1);
+        crystal1 = Math.floor(Math.random() * 11 + 1);
+        crystal2 = Math.floor(Math.random() * 11 + 1);
+        crystal3 = Math.floor(Math.random() * 11 + 1);
+        crystal4 = Math.floor(Math.random() * 11 + 1);
         playerTotal = 0;
         $('#playerScore').text(playerTotal);
     }
